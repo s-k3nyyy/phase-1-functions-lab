ccc32fb83b0c436782a473cc272b08c8c1ba671f
s-k3nyyy <collo.c.heg@gmail.com>
2024-03-22 09:48:25 +0300

    completed  lab

--- index.js
+++ index.js
@@ -1,31 +1,58 @@
-
-// Function to calculate distance in blocks from headquarters
-function distanceFromHqInBlocks(location) {
+//hq
+function distanceFromHqInBlocks(block) {
   const hq = 42;
-  return Math.abs(location - hq);
+  return Math(block - hq);
 }
 
-// Function to calculate distance in feet from headquarters
-function distanceFromHqInFeet(location) {
-  const blocks = distanceFromHqInBlocks(location);
-  return blocks * 264; // 1 block = 264 feet
+// distance in ft from HQ
+function distanceFromHqInFeet(block) {
+  const blocks = distanceFromHqInBlocks(block);
+  return blocks * 264;
+  //1bck = 264
 }
 
-// Function to calculate distance travelled in feet
-function distanceTravelledInFeet(start, destination) {
-  return Math.abs(destination - start) * 264; // 1 block = 264 feet
+// to calculate distance travelled in ft
+function distanceTravelledInFeet(startingpoint, destination) {
+  return Math(destination - startingpoint) * 264;
+  // 1 blk = 264 feet
 }
-
 // Function to calculate fare price
 function calculatesFarePrice(start, destination) {
   const distance = distanceTravelledInFeet(start, destination);
   if (distance <= 400) {
-    return 0; // Free sample
+    return 0;
   } else if (distance > 400 && distance <= 2000) {
-    return (distance - 400) * 0.02; // 2 cents per foot after first 400 feet
+    return (distance - 400) * 2 / 100; // 2 cts per foot after first 400 feet
   } else if (distance > 2000 && distance <= 2500) {
     return 25; // Flat rate for distance over 2000 feet
   } else {
-    return 'cannot travel that far'; // Distance limit exceeded
+    return 'we cant go that far';
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
